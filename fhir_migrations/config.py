44b8d352a13b3a43b27c2428e033e988b1f49d6e
Daniil <94884910+Filienko@users.noreply.github.com>
2024-06-14 20:38:00 -0700

    File

--- fhir_migrations/config.py
+++ fhir_migrations/config.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 # Get the path to the examples directory
-PROJECT_ROOT = Path(__file__)
+PROJECT_ROOT = Path(__file__).parent
 EXAMPLES_DIR = PROJECT_ROOT / "examples"
 
 MIGRATION_SCRIPTS_DIR = os.getenv("MIGRATION_SCRIPTS_DIR", str(PROJECT_ROOT))
